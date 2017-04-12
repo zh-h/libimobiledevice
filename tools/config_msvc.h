7e3c0e11e9fdb088e41764101d9696ef743af507
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-04-12 16:08:58 +0200

    Add S_ISDIR macro on Windows

--- tools/config_msvc.h
+++ tools/config_msvc.h
@@ -3,6 +3,10 @@
 #ifdef _MSC_VER
 #include <BaseTsd.h>
 typedef SSIZE_T ssize_t;
+
+#ifndef S_ISDIR
+#define S_ISDIR(mode)  (((mode)& S_IFMT) == S_IFDIR)
+#endif
 #endif
 
 #define PACKAGE_URL "https://github.com/libimobiledevice-win32"
